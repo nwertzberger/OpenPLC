0b1764bad6decde96847e0af4765c923b3d712b5
Nick Wertzberger <wertnick@gmail.com>
2011-06-08 22:46:33 -0500

    I realized that the main part of this project is the ethernet stack, and since the uIP port seems to have a decent project layout, I have officially hijacked it.

--- plc-sw/src/uip/libs/lcd/SPILCD.h
+++
@@ -1,8 +0,0 @@
-#ifndef __spilcd_H
-    #define __spilcd_H
-    void LCD_pulse_enable(void);
-    void SPILCD_init(void);
-    void LCD_sendData(uint8_t byte);
-    void LCD_sendCommand(uint8_t command);
-    void LCD_shiftout(uint8_t data);
-#endif
